9470c51538067cbaf0f8dd0d9d1a4ea6ae49950c
arttiko <augusto2barbosa@gmail.com>
2022-09-13 11:37:07 -0300

    update

--- assets/site/js/main.js
+++ assets/site/js/main.js
@@ -1,9 +1,45 @@
 
-/* Java de logando */
+
 $(document).ready(function () {
+	// login 
 	$("#login").click(function () {
 		$("#load").show();
 		$("#btn-acesso").hide();					
 	})
+
+	// slide inicial
+	var swiperparceiro = new Swiper(".slide-parceiro", {
+		loop: true,
+		slidesPerView: 5,
+		spaceBetween: 30,
+		freeMode: true,
+		autoplay: {
+			delay: 2500,
+			disableOnInteraction: false,
+		},
+		pagination: {
+			el: ".swiper-pagination",
+			clickable: true,
+		},
+	});
+
+	// slide parceiro
+	var swiper = new Swiper(".mySwiper", {
+		loop: true,
+		spaceBetween: 1200,
+		centeredSlides: true,
+		autoplay: {
+			delay: 2500,
+			disableOnInteraction: false,
+		},
+		pagination: {
+			el: ".swiper-pagination",
+			clickable: true,
+		},
+		navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev",
+		},
+	});
 })
 
